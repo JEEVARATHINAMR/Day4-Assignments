b0c21b51d44e9c8954eb2329c6998fc3e57cccb7
JEEVARATHINAMR <jeevasmartpkr@gmail.com>
2022-04-15 20:08:22 +0530

    printing Day4Assignments

---
+++ src/Day4Assignments/FirstDiceRoll.java
@@ -0,0 +1,17 @@
+package Day4Assignments;
+
+public class FirstDiceRoll {
+	
+	public static void main(String[] args) {
+		
+		 int position = 0, max = 6, min = 1;
+	     System.out.println("welcome player number 1");
+	     int rand_num = (int)Math.floor(Math.random()*(max-min+1)+min);
+	     System.out.println("first number rolled in dice: "+rand_num);
+	     
+		
+	}
+	
+	
+
+}
